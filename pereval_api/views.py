09b6524836dcc01f6c660292571edc093fb8f145
Lev Sergeev <levushka46@mail.ru>
2025-01-21 21:00:40 +0300

    End of 2 and 3 path

--- pereval_api/views.py
+++ pereval_api/views.py
@@ -3,19 +3,24 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import PerevalSerializer
 from .models import Pereval
+from .data_handler import DataHandler
 
 class SubmitDataView(APIView):
     def post(self, request):
         serializer = PerevalSerializer(data=request.data)
-        if serializer.is_valid():
-            pereval = serializer.save()
+        
+        if not serializer.is_valid():
             return Response({
-                'status': status.HTTP_200_OK,
-                'message': 'Отправлено успешно',
-                'id': pereval.id
-            }, status=200)
+                'status': status.HTTP_400_BAD_REQUEST,
+                'message': serializer.errors,
+                'id': None
+            }, status=400)
+        
+        # Используем DataHandler для работы с БД
+        result = DataHandler.create_pereval(serializer.validated_data)
+        
         return Response({
-            'status': status.HTTP_400_BAD_REQUEST,
-            'message': serializer.errors,
-            'id': None
-        }, status=400)
+            'status': result['status'],
+            'message': result['message'],
+            'id': result['id']
+        }, status=result['status'])
