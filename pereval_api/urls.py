d94b87d24b5a70a1c93f1e064c5fd737dd519392
Lev Sergeev <levushka46@mail.ru>
2025-01-22 21:14:19 +0300

    second sprint

--- pereval_api/urls.py
+++ pereval_api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import SubmitDataView
+from .views import SubmitDataView, PerevalDetailView, PerevalUpdateView, PerevalListView
 
 urlpatterns = [
     path('submitData/', SubmitDataView.as_view(), name='submit-data'),
+    path('submitData/<int:pk>/', PerevalDetailView.as_view(), name='pereval-detail'),
+    path('submitData/<int:pk>/update/', PerevalUpdateView.as_view(), name='pereval-update'),
+    path('submitData/list/', PerevalListView.as_view(), name='pereval-list'),
 ]
